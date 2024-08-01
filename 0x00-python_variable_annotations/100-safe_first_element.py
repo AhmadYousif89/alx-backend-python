46645c3b470f9960c4d1faa7a89c3bc5fe5bf025
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-01 15:40:51 +0300

    Add type annotations to safe_first_element function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+Type-annotated function safe_first_element that takes a lst of any type
+and returns its first element if exists, otherwise None.
+"""
+
+from typing import Sequence, Any, Union
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a sequence or None."""    
+    return lst[0] if lst else None
+        

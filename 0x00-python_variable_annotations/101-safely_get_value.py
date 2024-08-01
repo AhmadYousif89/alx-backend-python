2b200c7ecd3a3f47aaec752f353990f8b8d24a1b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-01 15:40:56 +0300

    Add type annotations to safely_get_value function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+Type-annotated function safely_get_value that takes a (dict, str) as argument
+and returns the value linked to key if exists, otherwise None.
+"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+D = Union[T, None]
+R = Union[Any, T]
+
+def safely_get_value(dct: Mapping, key: Any, default: D = None) -> R:
+    """Returns the value linked to key in a dict or None."""    
+    return dct[key] if key in dct else default

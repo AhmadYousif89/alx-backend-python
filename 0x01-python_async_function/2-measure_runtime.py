669ffeaa23c560914d2aeb193896a02f5722fd54
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-05 19:28:13 +0300

    Fix await in measure_time function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,10 +7,10 @@ from time import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def mesure_time(n: int, max_delay: int) -> float:
+def mesure_time(n: int, max_delay: int) -> float:
     """Measures the runtime of wait_n"""
     start = time()
-    await run(wait_n(n, max_delay))
+    run(wait_n(n, max_delay))
     end = time()
     elapsed = end - start
     return elapsed / n

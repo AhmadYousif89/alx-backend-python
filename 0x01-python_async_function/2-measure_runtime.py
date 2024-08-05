b657fa2510f97f3cc7d58b4a3074c53ff023e439
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-05 19:21:01 +0300

    Refactor code to use asyncio.run() for measuring runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Measure runtime for wait_n(n, max_delay)"""
 
-import asyncio
+from asyncio import run
 from time import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -10,7 +10,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def mesure_time(n: int, max_delay: int) -> float:
     """Measures the runtime of wait_n"""
     start = time()
-    await wait_n(n, max_delay)
+    await run(wait_n(n, max_delay))
     end = time()
     elapsed = end - start
     return elapsed / n

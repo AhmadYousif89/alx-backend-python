6d026a7d407cca8176fced910db1786e281424de
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-08-05 15:01:45 +0300

    Add asyncio import and use await in wait_random function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic asynchronous function"""
 
+import asyncio
 import random
 
 
@@ -10,4 +11,5 @@ async def wait_random(max_delay: int = 10) -> float:
     between 0 and max_delay seconds and returns it.
     """
     delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
